eff1aaac30b36c7388c89ff352f05afdc18c7c17
Sergejs Ivcenko <sergejs.ivcenko@gmail.com>
2020-12-06 16:28:49 +0000

    Added card action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               iMazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">
-              <i class="fas fa-shopping-basket"></i>
-            </a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              <i class="fas fa-shopping-cart"></i>
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Rating from "./Rating";
 import "./Product.css";
+import { Link } from "react-router-dom";
 
 export default function Product(props) {
   const { product } = props;
   return (
     <div key={product._id} className="card">
-      <a href={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
-      </a>
+      <Link to={`/product/${product._id}`}>
+        <div className="img-container">
+          <img
+            className="medium card__img"
+            src={product.image}
+            alt={product.name}
+          />
+        </div>
+      </Link>
       <div className="card__body">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <h2 className="card__title">{product.name}</h2>
-        </a>
+        </Link>
         <Rating
           rating={product.rating}
           numReviews={product.numReviews}
